3eb01654d269f2ef753dcc138f622286efab4f48
Tanusree Das Tithy <55296785+Tanusree-Das-Tithy@users.noreply.github.com>
2022-12-23 12:11:11 +0600

    Update Clock.js

--- components/Clock.js
+++ components/Clock.js
@@ -1,1 +1,40 @@
+import React from "react";
+import PropTypes from 'prop-types';
+const index=0;
+
+export class Clock extends React.Component {
+  state= {date:new Date()}; //same as constructor
+  /*constructor(props)
+  {
+    super(props);
+    this.state={date: new Date()};
+  }*/
+  componentDidMount(){
+    this.clockTimer= setInterval(()=>{
+     this.tick();
+    },1000);
+  }
+
+   componentWillUnmount(){
+        clearInterval(this.clockTimer);
+   }
+  tick(){
+    this.setState({
+      date:new Date(),
+     });
+  }
+  render(){
+  return (
+  <h1 className='heading' tabIndex={index}>
+    <span className='text'> {this.state.date.toLocaleTimeString(this.props.locale)}</span>
+  </h1>
+);
+}
+
+}
+Clock.propTypes = {
+  locale: PropTypes.any,
+};
+
+
 
