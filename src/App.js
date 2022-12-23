397d31133eb3706ad600ab8b3f2e9c1acda38f73
Tanusree Das Tithy <55296785+Tanusree-Das-Tithy@users.noreply.github.com>
2022-12-23 12:21:42 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Asset/css/global.css';
 //import Clock from './components/Clock';
+import {Clock} from './components/Clock';
 import MainComponent from './components/form';
 function App() {
   return <div className="Apps">
